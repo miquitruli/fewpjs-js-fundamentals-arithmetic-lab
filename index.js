855ba0023c41c9b89f049b5574226446a11077f9
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-06 14:53:13 -0500

    commit tree 528697b1e3f194abafb9e0a52caba59d99286d01

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+//is an equation whose multiplied results will equal 62
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2
+
+//generates a random integer greater than 0
+const random = Math.floor(Math.random())+1
+
+//is an equation that calculates a remainder that is equal to 4
+let num3 = 18;
+let num4 = 7;
+
+const mod = num3 % num4 // 7*2=14 18-14=4 4=remainder
+
+//will return 20 as the highest number in the set
+const max = Math.max(1, 2, 3, 4, 20)
